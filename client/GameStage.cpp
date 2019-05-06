e13f355f7e50b9ec6c02407dfa007269f5f69df2
EmbeddedJune <48465197+EmbeddedJune@users.noreply.github.com>
2019-05-07 01:49:16 +0900

    Update GameStage.cpp

--- client/GameStage.cpp
+++ client/GameStage.cpp
@@ -47,8 +47,10 @@ void CGameArea::OnFrameMove(float fElapsedTime) {
 
 void CGameStage::OnBeginMenu( DWORD dwObj )
 {
-	CGameStage* pGameStage = (CGameStage*)dwObj;
-	pGameStage->m_iCurMap = 0;
+	CGameStage* pGameStage = (CGameStage*)dwObj;	// 변수 선언
+	pGameStage->m_iCurMap = 0;			// 맵 커서 초기화
+	
+	// TODO - ini 파일명과 경로는 추후 달라질 수 있다는 점 명심!
 
 	g_pGameObjPool->LoadObj( L"Tile", L"Data/Tile.ini" );
 	g_pGameObjPool->LoadObj( L"Start", L"Data/UI/Menu/Start.ini" );
@@ -58,35 +60,16 @@ void CGameStage::OnBeginMenu( DWORD dwObj )
 	g_pGameObjPool->LoadObj( L"GameName", L"Data/UI/Menu/GameName.ini" );
 	g_pGameObjPool->LoadObj( L"GameImage", L"Data/UI/Menu/GameImage.ini" );
 	g_pGameObjPool->LoadObj( L"MenuBackGround1", L"Data/UI/Menu/MenuBackGround1.ini" );
-	g_pGameObjPool->LoadObj( L"MenuBackGround2", L"Data/UI/Menu/MenuBackGround2.ini" );
 	g_pGameObjPool->LoadObj( L"Loading", L"Data/UI/Loading/Loading.ini" );
-
-
-	//g_pGameObjMgr->UILoad( UI_NULL, L"GameName", L"UIMenu", D3DXVECTOR2( 12, 12 ), false, true );
-	//g_pGameObjMgr->UILoad( UI_NULL, L"GameImage", L"UIMenu", D3DXVECTOR2( 12, 152 ), false, true );
-	//g_pGameObjMgr->UILoad( UI_NULL, L"MenuBackGround1", L"UIMenu", D3DXVECTOR2( 580, 12 ), false, true );
-	//g_pGameObjMgr->UILoad( UI_NULL, L"MenuBackGround2", L"UIMenu", D3DXVECTOR2( 580, 556 ), false, true );
-	//g_pGameObjMgr->UILoad( UI_START, L"Start", L"UIMenu", D3DXVECTOR2( 596, 92 ), false, true );
-	////g_pGameObjMgr->UILoad( 0, L"Continue", L"UIMenu", D3DXVECTOR2( 596, 236 ), false, true );
-	////g_pGameObjMgr->UILoad( 0, L"Option", L"UIMenu", D3DXVECTOR2( 1270, 550 ), false, true );
-	////g_pGameObjMgr->UILoad( UI_EXIT, L"End", L"UIMenu", D3DXVECTOR2( 1270, 852 ), false, true );
-	//
-	//g_pGameObjPool->LoadAniObj( L"Mouse", L"Data/UI/Mouse/Mouse.ini" );
-	//g_pGameObjMgr->UILoad( UI_MOUSE, L"Mouse", L"Mouse", D3DXVECTOR2( 0, 0 ), true, true );
 
 }
 
 void CGameStage::OnEndMenu( DWORD dwObj )
 {
-	/*g_pGameObjMgr->Destroy( EGAMEOBJ_UI, L"UIMenu" );
-	g_pTexture->Release();*/
+
 }	
 
 void CGameStage::OnBeginNeos( DWORD dwObj )
 {
-	/*CGameStage* pGameStage = (CGameStage*)dwObj;
-	pGameStage->m_pGameArea = new CNeos_Area( pGameStage );
 
-	pGameStage->m_pGameArea->Load();
-	pGameStage->m_iCurMap = EMAP_NEOS;*/
 }
