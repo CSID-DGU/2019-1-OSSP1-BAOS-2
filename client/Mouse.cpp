c3b4760b71bf4fe30412b6a3a393823631508ef9
odyflame <ieehyeon@naver.com>
2019-05-31 23:21:36 +0900

    Add files via upload

--- client/Mouse.cpp
+++ client/Mouse.cpp
@@ -3,7 +3,7 @@
 
 extern CCamera* g_pCamera;
 
-CMouse::CMouse(void):
+CMouse::CMouse(void)://cmouse 생성 및 초기화
 	m_vRMousePos( NULLVECTOR2),
 	m_vLMousePos( NULLVECTOR2),
 	m_vOneRMousePos( NULLVECTOR2 ),
@@ -25,14 +25,14 @@ CMouse::~CMouse(void)
 }
 
 void CMouse::OnFrameMove( float fElapsedTime )
-{
+{//실시간으로 마우스의 위치를 갱신해준다.
 	SetMousePos();
 	SetWorldMousePos();
 	SetLMousePos( fElapsedTime );
 	SetRMousePos( fElapsedTime );
 }
 
-void CMouse::SetMousePos()
+void CMouse::SetMousePos()//마우스 위치 설정
 {
 	POINT ptMouse;
 	GetCursorPos( &ptMouse );
@@ -41,40 +41,43 @@ void CMouse::SetMousePos()
 	m_vMousePos = D3DXVECTOR2( (float)ptMouse.x, (float)ptMouse.y );
 }
 
-void CMouse::SetWorldMousePos()
+void CMouse::SetWorldMousePos()//월드 마우스 위치설정
 {
-	m_vWorldMousePos = m_vMousePos;
+	m_vWorldMousePos = m_vMousePos;	
+	//월드 마우스의 x좌표와 y좌표를 카메라의 좌표만큼 변경
+	//사실상 카메라의 위치
 	m_vWorldMousePos.x += ( g_pCamera->GetCameraPos().x  - (  g_pCamera->GetWidth() * 0.5f ) );
 	m_vWorldMousePos.y += ( g_pCamera->GetCameraPos().y  - (  g_pCamera->GetHeight() * 0.5f ) );
 }
 
 
-void CMouse::SetRMousePos( float fElapsedTime )
+void CMouse::SetRMousePos( float fElapsedTime )//오른족 마우스위치 설정
 {
-	m_vOneRMousePos = NULLVECTOR2;
-	m_bOneRClick = false;
+	m_vOneRMousePos = NULLVECTOR2;				//마우스 좌표 초기화
+	m_bOneRClick = false;						//클릭했는지 검사를 위한 bool자료형
 
 	m_fLMouseTime += fElapsedTime;
 
-	if( m_fRMouseTime > 4 * fElapsedTime  )
+	if( m_fRMouseTime > 4 * fElapsedTime  )		//클릭 시간이 설정한 시간보다 길 경우
 	{
-			m_bRClick = false;
-			m_vRMousePos = NULLVECTOR2;
-			m_fRMouseTime = 0;
-	}
-
-	if( KEYDOWN( VK_RBUTTON ) )
-	{
-		m_vRMousePos = m_vWorldMousePos;
+		//마우스 초기화
+		m_bRClick = false;
+		m_vRMousePos = NULLVECTOR2;
 		m_fRMouseTime = 0;
-		m_bRClick = true;
 	}
 
-	if( KEYDOWN( VK_RBUTTON ) == false )
+	if( KEYDOWN( VK_RBUTTON ) )			// 가상 키 코드 - 클릭이 성공한경우
 	{
-		if( m_bRClick == true )
+		m_vRMousePos = m_vWorldMousePos;// 마우스 위치를 월드마우스위치로 설정
+		m_fRMouseTime = 0;
+		m_bRClick = true;				// 클릭한 상황
+	}
+
+	if( KEYDOWN( VK_RBUTTON ) == false )// 가상 키 코드 - 클릭이 실패한경우
+	{
+		if( m_bRClick == true )			// 클릭한 경우
 		{
-			m_bRClick = false;
+			m_bRClick = false;			
 			m_bOneRClick = true;
 
 			m_vOneRMousePos = m_vRMousePos;
@@ -82,7 +85,7 @@ void CMouse::SetRMousePos( float fElapsedTime )
 	}
 }
 
-void CMouse::SetLMousePos( float fElapsedTime )
+void CMouse::SetLMousePos( float fElapsedTime )//위 코드와 마찬가지로 왼쪽 마우스 위치 설정
 {
 	m_vOneLMousePos = NULLVECTOR2;
 	m_bOneLClick = false;
--- client/Mouse.h
+++ client/Mouse.h
@@ -3,17 +3,17 @@
 class CMouse
 {
 private:
-	D3DXVECTOR2 m_vRMousePos;
-	D3DXVECTOR2 m_vLMousePos;
+	D3DXVECTOR2 m_vRMousePos;//오른쪽마우스
+	D3DXVECTOR2 m_vLMousePos;//왼쪽마우스
 
 	D3DXVECTOR2 m_vOneRMousePos;
 	D3DXVECTOR2 m_vOneLMousePos;
 
-	D3DXVECTOR2 m_vWorldMousePos;
+	D3DXVECTOR2 m_vWorldMousePos;//카메라 마우스위치
 	D3DXVECTOR2 m_vMousePos;
 
-	bool m_bRClick;
-	bool m_bLClick;
+	bool m_bRClick;//우클릭
+	bool m_bLClick;//좌클릭
 
 	bool m_bOneRClick;
 	bool m_bOneLClick;
@@ -21,9 +21,9 @@ private:
 	float m_fLMouseTime;
 	float m_fRMouseTime;
 
-private:
-	void SetMousePos();
-	void SetWorldMousePos();
+private://마우스 위치설정은 private로 지정
+	void SetMousePos();//마우스위치설정
+	void SetWorldMousePos();//월드마우스위치설정
 
 	void SetRMousePos( float fElapsedTime );
 	void SetLMousePos( float fElapsedTime );
@@ -41,13 +41,13 @@ public:
 	D3DXVECTOR2 GetOneRMousePos() { return m_vOneRMousePos; }
 	D3DXVECTOR2 GetOneLMousePos() { return m_vOneLMousePos; }
 
-	bool IsRClick() { return m_bRClick; }
+	bool IsRClick() { return m_bRClick; }//클릭했는지 검사
 	bool IsLClick() { return m_bLClick; }
 
 	bool IsOneRClick() { return m_bOneRClick; }
 	bool IsOneLClick() { return m_bOneLClick; }
 
 public:
-	void OnFrameMove( float fElapsedTime );
+	void OnFrameMove( float fElapsedTime );//프레임을 움직이면서 검사
 };
 
