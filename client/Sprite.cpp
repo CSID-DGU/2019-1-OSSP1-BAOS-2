3d0614a27dc4e894ce1c9277df4bcbec6039a385
odyflame <ieehyeon@naver.com>
2019-06-01 17:21:31 +0900

    Add files via upload

--- client/Sprite.cpp
+++ client/Sprite.cpp
@@ -1,7 +1,8 @@
 #include "DXUT.h"
 #include "Home.h"
 
-CSprite::CSprite(void):
+
+CSprite::CSprite(void)://스프라이트 생성
 m_pSprite( NULL )
 {
 }
@@ -9,48 +10,50 @@ m_pSprite( NULL )
 
 CSprite::~CSprite(void)
 {
-	SAFE_DELETE( m_pSprite );
+	SAFE_DELETE( m_pSprite );//스프라이트 소멸
 }
 
 void CSprite::OnResetDevice()
 {
-	D3DXCreateSprite( DXUTGetD3D9Device(), &m_pSprite );
+	D3DXCreateSprite( DXUTGetD3D9Device(), &m_pSprite );//directx 스프라이트 다시 생성
 }
 
 void CSprite::OnLostDevice()
 {
-	SAFE_RELEASE( m_pSprite );
+	SAFE_RELEASE( m_pSprite );//스프라이트를 놓아준다.
 }
 
 void CSprite::OnBegin()
 {
-	if( m_pSprite == NULL )
+	if( m_pSprite == NULL )//스프라이트가 NULL일때 그냥 리턴
 		return ;
 
-	m_pSprite->Begin( D3DXSPRITE_ALPHABLEND );
+	m_pSprite->Begin( D3DXSPRITE_ALPHABLEND );//directx sprite지원
 }
 
 void CSprite::OnEnd()
 {
-	if( m_pSprite == NULL )
+	if( m_pSprite == NULL )//스프라이트가 NULL일 때 return
 		return ;
 
-	m_pSprite->End();
+	m_pSprite->End();//directx 스프라이트 종료
 }
 
 void CSprite::SetTransform( D3DXMATRIX *pmatTrans )
 {
-	if( m_pSprite == NULL )
+	if( m_pSprite == NULL )//스프라이트가 NULL일 때 return
 		return ;
 
-	m_pSprite->SetTransform( pmatTrans );
+	m_pSprite->SetTransform( pmatTrans ); //pmatTrans로 스프라이트 전환 
+	//프로그램의 종료로 스프라이트가 사라질 때까지 무한루프
 }
 
-void CSprite::OnDraw( LPDIRECT3DTEXTURE9 pTexture
-	, const RECT * pRect
+void CSprite::OnDraw( LPDIRECT3DTEXTURE9 pTexture // 스프라이트 그려주기
+	, const RECT * pRect						  //텍스처와 그 영역을 받아준다.
 	, float fX
 	, float fY )
 {
-	D3DXVECTOR3 vPos( fX, fY, 0.0f );
+	D3DXVECTOR3 vPos( fX, fY, 0.0f );//vpos 위치 설정
 	m_pSprite->Draw( pTexture, pRect, &vPos, NULL, 0xffffffff );
+	//vpos위치에 텍스쳐를 그려준다.
 }
--- client/Sprite.h
+++ client/Sprite.h
@@ -1,6 +1,6 @@
 #pragma once
 
-class CSprite
+class CSprite//스프라이트, 사용자의 입력키, 좌표에 따라 원하는 행동이나 동작을 하는 이미지 출력
 {
 private:
 	LPD3DXSPRITE m_pSprite;
@@ -9,16 +9,16 @@ public:
 	CSprite(void);
 	virtual ~CSprite(void);
 
-	LPD3DXSPRITE GetSprite() { return m_pSprite; }
+	LPD3DXSPRITE GetSprite() { return m_pSprite; }//스프라이트 불러오기
 
-	void OnResetDevice();
-	void OnLostDevice();
+	void OnResetDevice();//디바이스 리셋
+	void OnLostDevice(); //디바이스 놓아줌
 
-	void OnBegin();
-	void OnEnd();
+	void OnBegin();		//시작
+	void OnEnd();		//끝
 
-	void SetTransform( D3DXMATRIX *pmatTrans );
-	void OnDraw( LPDIRECT3DTEXTURE9 pTexture
+	void SetTransform( D3DXMATRIX *pmatTrans );	//전환 설정
+	void OnDraw( LPDIRECT3DTEXTURE9 pTexture	//스프라이트 그려주기
 		, const RECT* pRect
 		, float fX
 		, float fY );
