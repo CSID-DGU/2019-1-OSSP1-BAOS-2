2eb9331917099737f4794890ae5fec5a67f7e845
odyflame <ieehyeon@naver.com>
2019-06-03 18:51:32 +0900

    Add files via upload

--- client/GameObjMgr.cpp
+++ client/GameObjMgr.cpp
@@ -10,74 +10,74 @@ CGameObjMgr::~CGameObjMgr(void)
 {
 }
 
-CGameObj* CGameObjMgr::Alloc( int iType )
+CGameObj* CGameObjMgr::Alloc(int iType)
 {
-	switch( iType )
+	switch (iType)//받아온 타입에 맞는 오브젝트 할당
 	{
-		case EGAMEOBJ_TILE: return new CTile;
-		case EGAMEOBJ_BEGINEFFECT: return new CEffect;
-		case EGAMEOBJ_PLAYER: return new CPlayer;
-		case EGAMEOBJ_CREATURE : return new CCreature;
-		case EGAMEOBJ_TOWER : return new CTower;
-		case EGAMEOBJ_HEALTH : return new CHealth;
-		case EGAMEOBJ_EFFECT : return new CEffect;
-		case EGAMEOBJ_DARK : return new CDark;
-		case EGAMEOBJ_MINIDARK : return new CDark;
+	case EGAMEOBJ_TILE: return new CTile;
+	case EGAMEOBJ_BEGINEFFECT: return new CEffect;
+	case EGAMEOBJ_PLAYER: return new CPlayer;
+	case EGAMEOBJ_CREATURE: return new CCreature;
+	case EGAMEOBJ_TOWER: return new CTower;
+	case EGAMEOBJ_HEALTH: return new CHealth;
+	case EGAMEOBJ_EFFECT: return new CEffect;
+	case EGAMEOBJ_DARK: return new CDark;
+	case EGAMEOBJ_MINIDARK: return new CDark;
 	}
 
 	return NULL;
 }
 
-void CGameObjMgr::OnFrameMove( float fElapsedTime )
+void CGameObjMgr::OnFrameMove(float fElapsedTime)//프레임무브 
 {
-	for each( map< int, vector< CGameObj* >>::const_reference mr in m_mapObj )
-	{
-		OnFrameMove( mr.second, fElapsedTime );
-
-		if( m_setPlayObj.find( mr.first ) != m_setPlayObj.end() )
-			for each( vector<CGameObj*>::const_reference vr in mr.second )
-				NoneOverLap( vr );
-	}
-
 	PostProcess();
+
+	for each(map< int, vector< CGameObj* >>::const_reference mr in m_mapObj)//map_obj의 프레임 렌더링
+	{
+		OnFrameMove(mr.second, fElapsedTime);
+
+		if (m_setPlayObj.find(mr.first) != m_setPlayObj.end())
+			for each(vector<CGameObj*>::const_reference vr in mr.second)
+				NoneOverLap(vr);
+	}
 }
 
-void CGameObjMgr::OnFrameMove( const std::vector<CGameObj*> &vecObj, float fElapsedTime)
+void CGameObjMgr::OnFrameMove(const std::vector<CGameObj*> &vecObj, float fElapsedTime)//프레임무브
 {
-	for each( vector<CGameObj*>::const_reference vr in vecObj )
+	for each(vector<CGameObj*>::const_reference vr in vecObj)
 	{
-		vr->OnFrameMove( fElapsedTime );
-	}	
+		vr->OnFrameMove(fElapsedTime);
+	}
 }
 
 void CGameObjMgr::OnFrameRender()
 {
-	SetRenderList();
+	SetRenderList();//렌더 리스트 설정
 
-	for each( map< int, vector<CGameObj*>>::const_reference mr in m_mapObj )
+	for each(map< int, vector<CGameObj*>>::const_reference mr in m_mapObj)
 	{
-		if( m_setPlayObj.find( mr.first ) == m_setPlayObj.end() )
-			OnFrameRender( mr.second );
+		if (m_setPlayObj.find(mr.first) == m_setPlayObj.end())
+			OnFrameRender(mr.second);
 		else
 			ListFrameRender();
 	}
 }
 
-void CGameObjMgr::OnFrameRender(const std::vector<CGameObj*> &vecObj)
+void CGameObjMgr::OnFrameRender(const std::vector<CGameObj*> &vecObj)//게임오브젝트 프레임 렌더링
 {
-	for each( vector<CGameObj*>::const_reference vr in vecObj )
-	{	
+	for each(vector<CGameObj*>::const_reference vr in vecObj)
+	{
 		vr->OnFrameRender();
 	}
 }
 
-void CGameObjMgr::ListFrameRender()
+void CGameObjMgr::ListFrameRender()//프레임렌더 리스트
 {
 	set< float >::iterator sit = m_setRenderList.begin();
-	for( sit; sit != m_setRenderList.end(); ++sit )
+	for (sit; sit != m_setRenderList.end(); ++sit)
 	{
-		multimap< float, CGameObj* >::iterator mit = m_mapRenderObj.find( *sit );
-		if( mit != m_mapRenderObj.end() )
+		multimap< float, CGameObj* >::iterator mit = m_mapRenderObj.find(*sit);
+		if (mit != m_mapRenderObj.end())
 			mit->second->OnFrameRender();
 	}
 }
--- client/GameObjMgr.h
+++ client/GameObjMgr.h
@@ -2,33 +2,36 @@
 
 enum
 {
-	EGAMEOBJ_TILE,
-	EGAMEOBJ_BEGINEFFECT,
+	EGAMEOBJ_TILE,//바닥 타일 오브젝트
+	EGAMEOBJ_BEGINEFFECT,//이펙트 시작 오브젝트 
 
-	EGAMEOBJ_TOWER,
-	EGAMEOBJ_PLAYER,
-	EGAMEOBJ_CREATURE,
+	EGAMEOBJ_TOWER,//건축물 오브젝트
+	EGAMEOBJ_PLAYER,//캐릭터 오브젝트
+	EGAMEOBJ_CREATURE,//크리쳐 오브젝트
 
-	EGAMEOBJ_EFFECT,
 
-	EGAMEOBJ_HEALTH,
-	EGAMEOBJ_DARK,
-	EGAMEOBJ_UI,
-	EGAMEOBJ_MINIDARK
+
+
+	EGAMEOBJ_EFFECT,//이펙트 오브젝트
+
+	EGAMEOBJ_HEALTH,//체력 오브젝트
+	EGAMEOBJ_DARK,//배경 오브젝트
+	EGAMEOBJ_UI,//UI 오브젝트
+	EGAMEOBJ_MINIDARK//미니맵 오브젝트
 };
 
-class CGameObjMgr : public CGameObjContainer
+class CGameObjMgr : public CGameObjContainer//게임오브젝트 컨테이너로부터 상속
 {
 private:
-	void OnFrameMove( const vector< CGameObj* >& vecObj, float fElapsedTime );
+	void OnFrameMove( const vector< CGameObj* >& vecObj, float fElapsedTime );//프레임무브 사용
 	void OnFrameRender( const vector< CGameObj* >& vecObj );
 
 protected:
-	virtual CGameObj* Alloc( int iType );
+	virtual CGameObj* Alloc( int iType );//게임오브젝트 할탕
 
 public:
-	CGameObj* pEnemyTower;
-	CGameObj* pFriendTower;
+	CGameObj* pEnemyTower;//적군 타워 게임 오브젝트 생성
+	CGameObj* pFriendTower;//아군 타워 게임 오브젝트 생성
 
 public:
 	CGameObjMgr(void);
@@ -38,3 +41,4 @@ public:
 	void OnFrameRender();
 	void ListFrameRender();
 };
+
