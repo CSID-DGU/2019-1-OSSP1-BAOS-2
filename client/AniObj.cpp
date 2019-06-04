7d23563628f959c752ae293e295fbf4598f35ab2
odyflame <ieehyeon@naver.com>
2019-06-04 16:03:00 +0900

    Add files via upload

--- client/AniObj.cpp
+++ client/AniObj.cpp
@@ -1,9 +1,9 @@
 #include "DXUT.h"
 #include "Home.h"
 
-extern CGameObjPool * g_pGameObjPool;
+extern GameDataMgr * g_pGameDataMgr;
 
-CAniObj::CAniObj(void):
+CAniObj::CAniObj(void)://게임 오브젝트 생성 및 초기화
 	m_iAniCategory( -1 ),
 	m_iInputIndex( -1 ),
 	m_iChangeIndex( -1 ),
@@ -19,37 +19,37 @@ CAniObj::~CAniObj(void)
 	UnLoad();
 }
 
-void CAniObj::Load( wstring wFile )
+void CAniObj::Load( wstring wFile )//wFile Load
 {
-	if( m_mapAniRender.empty() == false )
+	if( m_mapAniRender.empty() == false )//aniRender가 비어있을 경우 종료
 		return ;
 
-	// GameObjPool에서 애니메이션 관련 데이터를 wfile에서 받아온다.
-	// 값을 변경하지 않을 예정이므로 const 처리 해주는 것 잊지말기.
-	const SAniObjData* pObjData = g_pGameObjPool->GetAniObjData( wFile );	
+	const SAniObjData* pObjData = g_pGameDataMgr->GetAniObjData( wFile );
 	if( pObjData == NULL )
 		return ;
 
-	SetScl( D3DXVECTOR2( pObjData->m_vScale ));	
-	// ini파일에 저장되어있는 애니메이션 정보들을 모두 읽어 초기화한다.
-	m_iMaxHealth = pObjData->m_iMaxHealth;	// 최대 체력을 받아온다.
-	m_iHealth = pObjData->m_iHealth;		// 현재 체력을 받아온다.
 
-	m_iDamage = pObjData->m_iDamage;		// 데미지를 받아온다.
-	m_fSpeed = pObjData->m_fSpeed;			// 이동속도를 받아온다
+	SetScl( D3DXVECTOR2( pObjData->m_vScale ));//데이터 오브젝트의 스케일 설정
 
-	m_iScope = pObjData->m_iScope;			// 공격범위를 받아온다.	
+	//오브젝트 데이터의 최대체력, 체력, 데미지 상태, 스피드, 범위, 반지름 설정
+	m_iMaxHealth = pObjData->m_iMaxHealth;
+	m_iHealth = pObjData->m_iHealth;
+
+	m_iDamage = pObjData->m_iDamage;
+	m_fSpeed = pObjData->m_fSpeed;
+
+	m_iScope = pObjData->m_iScope;
 	m_iRadius = pObjData->m_iRadius;
 
-	map< int, SAniData >::const_iterator mit = pObjData->m_mapAni.begin();
-	for( mit; mit != pObjData->m_mapAni.end(); ++mit )
+	map< int, SAniData >::const_iterator mit = pObjData->m_mapAni.begin();//오브젝트 데이터 이터레이터 설정
+	for( mit; mit != pObjData->m_mapAni.end(); ++mit )//오브젝트 데이트만큼 반복
 	{
-		if( mit->second.m_wFile.empty() )
+		if( mit->second.m_wFile.empty() )//비어있을 경우 지나감
 			continue;
 
 		CRender* pRender;
 
-		switch( m_iRenderType )				// 애니메이션 타입에 맞게 Rendering 해준다.
+		switch( m_iRenderType )//타입에 따라 렌더링해줌 
 		{
 		case ERENDER_NORMAL:
 			{
@@ -73,21 +73,22 @@ void CAniObj::Load( wstring wFile )
 	}
 }
 
-void CAniObj::UnLoad()
+void CAniObj::UnLoad()//로드 해제
 {
 	m_pRender = NULL;
 
+	//Map에 저장되어 있는 AniRender들을 전부 안전하게 제거
 	map< int, CRender* >::iterator mit = m_mapAniRender.begin();
 	for( mit; mit != m_mapAniRender.end(); ++mit )
-		SAFE_DELETE( mit->second );
+		SAFE_DELETE( mit->second );//안전하게 제거
 
-	if( m_pController != NULL )
+	if( m_pController != NULL )//컨트롤러도 제거한다.
 		SAFE_DELETE( m_pController );
 
 	m_mapAniRender.clear();
 }
 
-void CAniObj::OnFrameMove( float fElapsedTime )
+void CAniObj::OnFrameMove( float fElapsedTime )//실시간으로 프레임 갱신
 {
 	if( m_pRender == NULL )
 		return ;
@@ -98,66 +99,69 @@ void CAniObj::OnFrameMove( float fElapsedTime )
 	if( m_pRender->OnFrameMove( fElapsedTime ) == false )
 		OnEndAni();
 
-	if( m_bChangeNextAni )
+	if( m_bChangeNextAni )//다음 애니메이션으로 전환
 	{
 		if( (( CDirectionAniRender* ) m_pRender )->GetAniIndex() >= m_iChangeIndex )
 			OnChangeAni( m_iNextAniCategory );
 	}
 }
 
-void CAniObj::OnEndAni()
+void CAniObj::OnEndAni()//Ani 종료
 {
 	if( m_pController != NULL )
 		m_pController->OnEndAni( m_iAniCategory );
 }
 
-void CAniObj::OnFrameRender()
+void CAniObj::OnFrameRender()//Frame Rendering
 {
 	if( m_pRender == NULL )
 		return ;
 
-	m_pRender->OnFrameRender();
+	m_pRender->OnFrameRender();//프레임 렌더링
 }
 
-void CAniObj::OnChangeAni( int iAni )
+void CAniObj::OnChangeAni( int iAni )//Ani 전환
 {
-	map< int, CRender* >::iterator mit = m_mapAniRender.find( iAni );
-	if( mit == m_mapAniRender.end() )
+	map< int, CRender* >::iterator mit = m_mapAniRender.find( iAni );//iAni에 찾아서 iterator 설정
+	if( mit == m_mapAniRender.end() )//찾지 못했을 경우
 	{
 		Destroy();
 		return ;
 	}
 
+	//애니 전환 준비과정, 초기화
 	m_iInputIndex = -1;
 	m_iChangeIndex = -1;
 	m_iNextAniCategory = -1;
 
+	//애니 카테코리 재설정
 	m_iAniCategory = iAni;
 
 	m_pRender = mit->second;
 	((CDirectionAniRender*)m_pRender)->SetAniIndex( 0 );
 
-	m_pRender->UpdateWorld();
+	m_pRender->UpdateWorld();//게임 다시 갱신
 
-	if( m_pController != NULL )
+	if( m_pController != NULL )//컨트롤러 애니 전환
 		m_pController->OnChangeAni( iAni );
 
 }
 
-void CAniObj::SetNextAni( int iInputIndex, int iChangeIndex, int iAniCategory )
+void CAniObj::SetNextAni( int iInputIndex, int iChangeIndex, int iAniCategory )//다음 애니 설정
 {
+	//받아온 인수들로 index와 카테코리 설정
 	m_iInputIndex = iInputIndex;
 	m_iChangeIndex = iChangeIndex;
 	m_iAniCategory = iAniCategory;
 }
 
-void CAniObj::TryNextAni()
+void CAniObj::TryNextAni()//다음 애니메이션으로 전환시도
 {
-	if( m_iNextAniCategory == NULL )
+	if( m_iNextAniCategory == NULL )//다음 애니 카테고리가 없을경우 종료
 		return ;
 
-	if( ((CDirectionAniRender*)m_pRender)->GetAniIndex() < m_iInputIndex )
+	if( ((CDirectionAniRender*)m_pRender)->GetAniIndex() < m_iInputIndex )//받은 index가 가지고 있는 애니 인덱스보다 클경우
 		return ;
 
-	m_bChangeNextAni = true;
+	m_bChangeNextAni = true;//전환성공확인용 bool 자료형
 }
--- client/AniObj.h
+++ client/AniObj.h
@@ -2,22 +2,22 @@
 
 class CGameObjController;
 
-class CAniObj : public CGameObj 
+class CAniObj : public CGameObj //게임 오브젝트 상속
 {
 protected:
-	int m_iAniCategory;
-	map< int, CRender* > m_mapAniRender;
+	int m_iAniCategory;//애니 카테고리 index
+	map< int, CRender* > m_mapAniRender;//Ani render 저장할 map 구조체
 
-	int m_iInputIndex;
-	int m_iChangeIndex;
-	int m_iNextAniCategory;
+	int m_iInputIndex;//입력받은 index
+	int m_iChangeIndex;//index 변환
+	int m_iNextAniCategory;//다음 Ani index
 
-	CGameObjController* m_pController;
+	CGameObjController* m_pController;//게임 오브젝트 컨트롤러 생성
 
-	bool m_bChangeNextAni;
+	bool m_bChangeNextAni;//다음 Ani로 변환 결정 위한 bool형
 
 protected:
-	virtual void OnEndAni();
+	virtual void OnEndAni();//Ani 종료
 
 public:
 	CAniObj(void);
@@ -32,7 +32,7 @@ public:
 	CGameObjController* GetController() { return m_pController; }
 
 	void OnChangeAni( int iAni );
-	int GetAniCategory() { return m_iAniCategory; }
+	virtual int GetAniCategory() { return m_iAniCategory; }
 
 	void SetNextAni( int iInputIndex, int iChangeIndex, int iAniCategory );
 	void TryNextAni();
