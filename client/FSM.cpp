00527645eb8943a4c2cf7a841274e07738d907b2
odyflame <ieehyeon@naver.com>
2019-06-04 15:47:52 +0900

    Add files via upload

--- client/FSM.cpp
+++ client/FSM.cpp
@@ -8,57 +8,58 @@ m_pCurState( NULL ),
 {
 }
 
+
 CFSM::~CFSM(void)
 {
 }
 
-void CFSM::AddState( int iState, const SFSMState& pState )
+void CFSM::AddState( int iState, const SFSMState& pState )//상태 추가
 {
 	map< int, SFSMState >::iterator mit = m_mapState.find( iState );
-	if( mit != m_mapState.end() )
+	if( mit != m_mapState.end() )//이미 상태 있을 경우 return
 		return ;
 
-	m_mapState.insert( make_pair( iState, pState ));
+	m_mapState.insert( make_pair( iState, pState ));//상태들이 저장된 map에 추가
 }
 
-SFSMState* CFSM::GetState( int iState )
+SFSMState* CFSM::GetState( int iState )//상태 불러오기
 {
-	map< int, SFSMState >::iterator mit = m_mapState.find( iState );
-	if( mit == m_mapState.end() )
+	map< int, SFSMState >::iterator mit = m_mapState.find( iState );//mapState itorator 설정
+	if( mit == m_mapState.end() )//찾지 못했을 경우 NULL return
 		return NULL;
 
 	return &mit->second;
 }
 
-bool CFSM::OnChangeState( int iState, bool bReserve )
+bool CFSM::OnChangeState( int iState, bool bReserve )//상태 전환
 {
-	m_pNextState = GetState( iState );
-	if( m_pNextState == NULL )
+	m_pNextState = GetState( iState );//다음 상태 불러옴
+	if( m_pNextState == NULL )//다음 상태가 없을 경우 false return
 		return false; 
 
 	if( bReserve == false )
 		OnChangeReserveState();
 
-	return true;
+	return true;//상태 전환 성공
 }
 
-void CFSM::OnChangeReserveState()
+void CFSM::OnChangeReserveState()//
 {
-	if( m_pNextState == NULL )
+	if( m_pNextState == NULL )//다음 상태가 없는 경우 종료
 		return ;
 
-	if( m_pCurState != NULL && m_pCurState->m_pfEnd != NULL )
-		( m_pCurState->m_pfEnd )( m_pCurState->m_dwArgs );
+	if( m_pCurState != NULL && m_pCurState->m_pfEnd != NULL )//현재 상태가 존재할 경우
+		( m_pCurState->m_pfEnd )( m_pCurState->m_dwArgs );//현재 상태의 끝을 가리키는 쪽으로 전환  
 
-	m_pCurState = m_pNextState;
+	m_pCurState = m_pNextState;//현재 상태를 다음 상태로 전환
 	m_pNextState = NULL;
 
 	if( m_pCurState->m_pfBegin != NULL )
 		( m_pCurState->m_pfBegin )( m_pCurState->m_dwArgs );
 }
 
-void CFSM::OnUpdateState( float fElapsedTime )
+void CFSM::OnUpdateState( float fElapsedTime )//현재 상태 갱신
 {
-	if( m_pCurState != NULL && m_pCurState->m_pfUpdate != NULL )
-		( m_pCurState->m_pfUpdate )( m_pCurState->m_dwArgs, fElapsedTime );
+	if( m_pCurState != NULL && m_pCurState->m_pfUpdate != NULL )//현 상태가 존재하고 업데이트할 상태가 존재할 경우
+		( m_pCurState->m_pfUpdate )( m_pCurState->m_dwArgs, fElapsedTime );//현재 상태 없데이트
 }
--- client/FSM.h
+++ client/FSM.h
@@ -1,18 +1,20 @@
 #pragma once
 
-struct SFSMState
+struct SFSMState//FSM 정보 저장용 구조체
 {
-	typedef void (*PFBEGIN)(DWORD);//DWord == unsigned long
-	typedef void (*PFUPDATE)(DWORD , float );
-	typedef void (*PFEND)(DWORD);
+	//다형성 위한 함수 타입화 
+	typedef void (*PFBEGIN)(DWORD);//
+	typedef void (*PFUPDATE)(DWORD , float );//업데이트
+	typedef void (*PFEND)(DWORD);//몬스터 죽거나 게임 종료
 
-	PFBEGIN m_pfBegin;
-	PFUPDATE m_pfUpdate;
-	PFEND m_pfEnd;
+	//상태별 구조체 형설
+	PFBEGIN m_pfBegin;//처음 상태 
+	PFUPDATE m_pfUpdate;//업데이트 상태
+	PFEND m_pfEnd;//종료 상태
 
 	DWORD m_dwArgs;
 
-	SFSMState( DWORD dwArgs, PFBEGIN pfBegin, PFUPDATE pfUpdate, PFEND pfEnd ):
+	SFSMState( DWORD dwArgs, PFBEGIN pfBegin, PFUPDATE pfUpdate, PFEND pfEnd )://초기화 부분
 	m_dwArgs( dwArgs ),
 		m_pfBegin( pfBegin ),
 		m_pfUpdate( pfUpdate ),
@@ -24,20 +26,20 @@ struct SFSMState
 class CFSM
 {
 private:
-	map< int, SFSMState > m_mapState;
+	map< int, SFSMState > m_mapState;//맵 상태 map자료구조에 저장
 protected:
-	SFSMState* m_pCurState;
-	SFSMState* m_pNextState;
+	SFSMState* m_pCurState;//현재상태 FSM
+	SFSMState* m_pNextState;//다음상태 FSM
 public:
 	CFSM(void);
 	virtual ~CFSM(void);
 
-	void AddState( int iState, const SFSMState& pState );
-	SFSMState* GetState( int iState );
+	void AddState( int iState, const SFSMState& pState );//상태 추가
+	SFSMState* GetState( int iState );//상태 정보 불러오기
 
 	bool CFSM::OnChangeState( int iState, bool bReserve = true );
-	void OnChangeReserveState();
+	void OnChangeReserveState();//예약된 상태 전환
 
-	void OnUpdateState( float fElapsedTime );
+	void OnUpdateState( float fElapsedTime );//상태 업데이트
 };
 
