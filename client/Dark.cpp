bc7203e202cc6a4a1be11623a6019a3330f79708
odyflame <ieehyeon@naver.com>
2019-06-07 18:18:51 +0900

    Add files via upload

--- client/Dark.cpp
+++ client/Dark.cpp
@@ -2,66 +2,91 @@
 #include "Home.h"
 
 extern CGameObjMgr* g_pGameObjMgr;
-extern CGameObjPool* g_pGameObjPool;
+extern GameDataMgr* g_pGameDataMgr;
 extern CTexture* g_pTexture;
 
-CDark::CDark(void):
-m_iViewID( 0 )
+CDark::CDark(void) ://Dark 클래스에 있는 변수 초기화
+	m_iViewID(0),
+	m_fDarkUpdateTimer(0.0f),
+	m_fDarkResetTimer(GameSettingIndex::UpdateDarkResetTime)
 {
 }
 
 
-CDark::~CDark(void)
+CDark::~CDark(void)//소멸자
 {
 }
 
-void CDark::Load( wstring wFile )
+void CDark::Load(wstring wFile)
 {
-	if( m_pRender != NULL )
-		return ;
+	if (m_pRender != NULL)//Render할 것이 있는경우
+		return;
 
-	const SObjData* pObjData = g_pGameObjPool->GetObjData( wFile );
-	if( pObjData == NULL )
-		return ;
+	const SObjData* pObjData = g_pGameDataMgr->GetObjData(wFile);//wFile이름의 오브젝트 데이터 정보 불러옴
+	if (pObjData == NULL)//오브젝트 데이터를 못불러왔을경우
+		return;
 
-	/*if( m_wObjName == L"Dark" )
+	if (m_wObjName == L"Dark")//오브젝트 이름이 Dark인경우
 	{
 		m_vScl = pObjData->m_vScale;
-		m_pRender = new CImgRender( this );
+		m_pRender = new CDarkRender(this);//Dark Render 생성
 	}
 
-	if( m_wObjName == L"MiniDark" )
+	if (m_wObjName == L"MiniDark")//오브젝트가 미니맵인경우
 	{
-		m_vPos += D3DXVECTOR2( 768, 512 );
-		m_vScl = m_vScl * 2;
-		m_pRender = new CUIImgRender( this );
+		m_vPos += D3DXVECTOR2(768, 512);//위치에서 기준만큼 오브젝트 위치 이동
+		m_vScl = m_vScl * 2;//스케일 설정
+		m_pRender = new CDarkRender(this);
+		m_bCamera = false;
 	}
 
-
-	m_pRender->Load( pObjData->m_wFile );
-
-	
-	m_iViewID = g_pTexture->Load( L"Image/UI/View/CreatureView (0).png" );*/
+	m_pRender->Load(pObjData->m_wFile);//wFile의 오브젝트 데이터를 로드
+	m_iViewID = g_pTexture->Load(L"Image/UI/View/CreatureView (0).png");//viewID를 이미지 위치에서 로드
 }
 
-void CDark::OnFrameMove( float fElapsedTime )
+void CDark::ResetDark()//Dark reset
 {
-	/*if( m_pRender != NULL )
-		m_pRender->OnFrameMove( fElapsedTime );
+	m_fDarkResetTimer = GameSettingIndex::UpdateDarkResetTime;//Dark resetTime 설정
+	((CDarkRender*)m_pRender)->ClearDark();
+}
 
-
-	vector< CGameObj* > m_vecViewObj = g_pGameObjMgr->GetViewObj();
-
-	if( m_vecViewObj.empty() == false )
+void CDark::UpdateDark()//Dark Update
+{
+	m_fDarkUpdateTimer = GameSettingIndex::UpdateDarkTime;//Dark updatetime 설정
+	vector< CGameObj* > m_vecViewObj = g_pGameObjMgr->GetViewObj();//게임 view 오브젝트 정보를 게임오브젝트매니저에서 불러옴
+	if (m_vecViewObj.empty() == false)//비어있지 않을 경우
 	{
-		for( int i = 0; i <= (int)m_vecViewObj.size() - 1; ++ i )
+		for (int i = 0; i <= (int)m_vecViewObj.size() - 1; ++i)
 		{
-			if( m_vecViewObj[i]->IsDestroy() == false )
+			if (m_vecViewObj[i]->IsDestroy() == false)//제거되지 않았을 경우
 			{
-				D3DXVECTOR2 vViewPos = *m_vecViewObj[i]->GetPos() / 16;
-
-				m_pRender->SetDarkMode( vViewPos , m_iViewID );
+				D3DXVECTOR2 vViewPos = *m_vecViewObj[i]->GetPos() / 16;//view 위치 지정
+				((CDarkRender*)m_pRender)->SetDarkMode(vViewPos, m_iViewID);
 			}
 		}
-	}*/
+	}
+}
+
+
+void CDark::OnFrameMove(float fElapsedTime)//프레임 갱신
+{
+	if (m_pRender != NULL)//렌더가 할 것이 있을경우
+		m_pRender->OnFrameMove(fElapsedTime);//프레임 갱신
+
+	if (m_pRender == nullptr)
+		return;
+
+	m_fDarkUpdateTimer -= fElapsedTime;//업데이트 타이머에서 현재시간 갱신
+	m_fDarkResetTimer -= fElapsedTime;//리셋 타이머에서 현재시간 갱신 
+
+	if (m_fDarkResetTimer <= 0.0f)//리셋타이머가 0이하일 경우 
+	{
+		ResetDark();//게임 리셋후 다시 업데이트
+		UpdateDark();
+	}
+
+	if (m_fDarkUpdateTimer <= 0.0f)
+	{
+		UpdateDark();//게임 업데이트
+	}
 }
--- client/Dark.h
+++ client/Dark.h
@@ -5,9 +5,16 @@ class CDark : public CGameObj
 private:
 	int m_iViewID;
 
+private:
+	float m_fDarkUpdateTimer;
+	float m_fDarkResetTimer;
+
 public:
 	CDark(void);
 	virtual ~CDark(void);
+
+	void UpdateDark();
+	void ResetDark();
 
 	virtual void Load( wstring wFile );
 	virtual void OnFrameMove( float fElapsedTime );
