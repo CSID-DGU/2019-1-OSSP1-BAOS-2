d4401c00e8da8c66f1138b2b3f3c20867a6dca6d
odyflame <ieehyeon@naver.com>
2019-06-14 19:40:48 +0900

    Add files via upload

---
+++ client/GameSettingIndex.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace GameSettingIndex
+{
+	const float CreateSmallCreatureTime = 2.0f;//작은 크리쳐 생성시 쿨타임
+	const float CreateBigCreatureTime = 10.0f;//큰 크리쳐 생성시 쿨타임
+
+	const float UpdateDarkTime = 0.5f;//게임 업데이트 시간
+	const float UpdateDarkResetTime = 3.0f;//게임 리셋 시간
+	
+	const float SkillCoolTime1 = 1.0f;//스킬 쿨타임
+	const float SkillCoolTime2 = 1.0f;
+};
